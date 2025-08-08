7cf3d0a24d01c0e25ecfe3b297a187dee52ff4f1
kazin77990 rodrigus <reliquiavr@gmail.com>
2025-08-08 17:38:43 +0000

    atualização do crud categoria

--- crud/categoria.py
+++ crud/categoria.py
@@ -1,27 +1,39 @@
 from db_confing import conecatar
 
 def criar_categoria(nome, descricao):
-    conn = conecatar()
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO categotia (nome, descricao) VALUES (%s, %s) , (nome, descriacao)")
-    conn.commit()
-    conn.close()
-    print("Categoria criada com sucesso!")
-
-    def listar_categoria():
+    try:  
+        conn = conecatar()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO categotia (nome, descricao) VALUES (%s, %s) , (nome, descriacao)")
+        conn.commit()
+        return{"status": "mensagem""categoria criada com sucesso!"}
+    except Exception as e:
+        return{"status":"erro","mensagem":str(e)}
+        conn.close()
+        
+def listar_categoria():
+    try:
         conn = conecatar()
         cursor = conn.cursor(dictionary=True)
         cursor.execute("SELECT * FROM categoria")
-        categorias = cursor.fetchall()
+        return cursor.fetchall()
+    except Exception as e:
+        return{"status":"erro","mensagem":str(e)}
+    finally:
         conn.close()
-        return categorias
+     
 
 def atualizar_categoria(id_categoria, novo_nome, nova_discricao):
-    conn = conecatar()
-    cursor = conn.cursor()
-    cursor.execute("UPDATE categoria SET nome=%s, descricao=%s WHER id=%s",
-                    (novo_nome, nova_discricao, id_categoria))
-    conn.commit()
+    try:
+        conn = conecatar()
+        cursor = conn.cursor()
+        cursor.execute("UPDATE categoria SET nome=%s, descricao=%s WHER id=%s",
+                (novo_nome, nova_discricao, id_categoria))
+        conn.commit()
+        if cursor.rowcount == 0:
+          return{"status":"sucesso","mensagem":"Nenhuma categoria encontrada para atualizar."}
+        return{("status""aviso","mensagem":"categoria atualizar")}
+    except Exception as e:
     conn.close()
     print("categoria(id_atualizada!")
 
